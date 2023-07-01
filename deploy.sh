744bcf29aa51a904799f8be3a02faa2352eeeefb
babyboym <ifeanyiogbechie@gmail.com>
2023-07-01 17:08:37 +0100

    updated..

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@ This is a deployment server.
 Becareful with the configurations
 hello ladies and gentlemen
 How are you doing.
-Its a pleasure addressing you today.
+Its a pleasure addressing you today
+Thanks guys
+Its my pleasure.
